d5d470b68dffb17de732e388dd730d78a5542f86
adegard <adegard@users.noreply.github.com>
2026-08-22 15:06:37 +0200

    fix: missing FrameLayout and lifecycleScope imports

--- app/src/main/java/com/adegard/meteocesate/MainActivity.kt
+++ app/src/main/java/com/adegard/meteocesate/MainActivity.kt
@@ -8,12 +8,14 @@ import android.os.Bundle
 import android.view.Gravity
 import android.view.View
 import android.widget.Button
+import android.widget.FrameLayout
 import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.lifecycleScope
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import kotlinx.coroutines.launch
 
